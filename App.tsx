5b1c0c816745ba66fa864ab6a9adedd61bc2c22a
LucasHaberles <lucashaberles41q@gmail.com>
2023-08-05 19:45:10 -0300

    agregación de typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hola mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF7800',
     alignItems: 'center',
+    fontSize: 84,
     justifyContent: 'center',
   },
+  text: {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: 24,
+  }
 });
